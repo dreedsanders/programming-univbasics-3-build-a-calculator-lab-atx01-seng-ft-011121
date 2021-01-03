a2255db6c1045c0a7bd4e45a7352e033448bba43
kooky-range-8610 <dreedsanders@gmail.com>
2021-01-03 18:40:49 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,12 +3,17 @@ num2 = num2.to_i
 
 
 def addition (num1, num2)
- puts num1 + num2
+num1 = num1.to_i 
+num2 = num2.to_i
+puts num1 + num2
 end
 
 addition "5" , "4"
 
 def subraction (num1, num2)
-  puts num1 - num2
+num1 = num1.to_i 
+num2 = num2.to_i
+puts num1 - num2
 end
+
 subraction "50", "25"
